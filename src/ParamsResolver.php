b7685fbb59db6a42d518d546d7cb10fe2f103bdf
pine3ree <pine3ree@gmail.com>
2025-04-27 23:40:46 +0200

    Refactoring! Split the resolve method into two separate methods: resolve-reflection-parameters and resolve-arguments methods. ALso treat invokable object as separate case for clarity
    Signed-off-by: pine3ree <pine3ree@gmail.com>

--- src/ParamsResolver.php
+++ src/ParamsResolver.php
@@ -37,7 +37,7 @@ class ParamsResolver implements ParamsResolverInterface
     private ContainerInterface $container;
 
     /**
-     * A cache of resolved reflection parameters indexed by function/method name
+     * A cache of resolved reflection parameters indexed by function/class::method name
      *
      * @var array<string, ReflectionParameter[]>
      */
@@ -50,19 +50,175 @@ class ParamsResolver implements ParamsResolverInterface
 
     public function resolve($callable, ?array $resolvedParams = null): array
     {
+        $rf_params = $this->resolveReflectionParameters($callable);
+
+        if (empty($rf_params)) {
+            return [];
+        }
+
+        return $this->resolveArguments($rf_params, $resolvedParams);
+
+//        // Check if closure or invokable objects
+//        $is_object    = is_object($callable);
+//        $is_closure   = $is_object && $callable instanceof Closure;
+//        $is_invokable = $is_object && !$is_closure && method_exists($callable, '__invoke');
+//
+//        // Only invokable objects are transformed into array form
+//        // Closures are handled directly via reflection api
+//        if ($is_invokable) {
+//            $callable = [$callable, '__invoke'];
+//        }
+//
+//        // Fetch the reflection function/method parameters
+//        if (is_array($callable)) {
+//            $object = $callable[0];
+//            $method = $callable[1];
+//            if (is_object($object)) {
+//                $class = get_class($object);
+//            } else {
+//                $class = $object;
+//                if (!class_exists($class)) {
+//                    throw new RuntimeException(
+//                        "A class named `{$class}::{$method}` is not defined for given callable!"
+//                    );
+//                }
+//            }
+//            // Try cached reflection parameters first, if any
+//            $cache_key = "{$class}::{$method}";
+//            $rf_params = self::$rf_params[$cache_key] ?? null;
+//            if ($rf_params === null) {
+//                if ($is_invokable) {
+//                    $rm = new ReflectionMethod($class, '__invoke');
+//                } else {
+//                    $rc = new ReflectionClass($class);
+//                    if (!$rc->hasMethod($method)) {
+//                        throw new RuntimeException(
+//                            "A method named `{$class}::{$method}` is not defined for given callable!"
+//                        );
+//                    }
+//                    $rm = $method === '__construct' ? $rc->getConstructor() : $rc->getMethod($method);
+//                }
+//                if ($rm instanceof ReflectionMethod) {
+//                    self::$rf_params[$cache_key] = $rf_params = $rm->getParameters();
+//                }
+//            }
+//        } elseif (is_string($callable) && function_exists($callable)) {
+//            $rf_params = self::$rf_params[$callable] ?? null;
+//            if ($rf_params === null) {
+//                $rf = new ReflectionFunction($callable);
+//                self::$rf_params[$callable] = $rf_params = $rf->getParameters();
+//            }
+//        } elseif ($is_closure) {
+//            $rf = new ReflectionFunction($callable);
+//            $rf_params = $rf->getParameters();
+//        } else {
+//            throw new RuntimeException(
+//                "Cannot fetch a reflection method or function for given callable!"
+//            );
+//        }
+//
+//        if (empty($rf_params)) {
+//            return [];
+//        }
+//
+//        $container = $this->container;
+//
+//        // Build the arguments for the provided ~callable~
+//        $args = [];
+//        /** @var ReflectionParameter $rp */
+//        foreach ($rf_params as $rp) {
+//            $rp_name = $rp->getName();
+//            $rp_type = $rp->getType();
+//            if ($rp_type instanceof ReflectionNamedType && !$rp_type->isBuiltin()) {
+//                $rp_fqcn = $rp_type->getName();
+//                // fqcn/fqin type-hinted arguments
+//                if (interface_exists($rp_fqcn) || class_exists($rp_fqcn)) {
+//                    if (isset($resolvedParams[$rp_fqcn])) {
+//                        // Try injected/resolved params first
+//                        $args[] = $resolvedParams[$rp_fqcn];
+//                    } elseif ($rp_fqcn === ContainerInterface::class || $rp_fqcn === get_class($container)) {
+//                        // A container should not be a type-hinted dependency (service-locator anti-pattern),
+//                        // but another dependency resolver might use it
+//                        $args[] = $container;
+//                    } elseif ($container->has($rp_fqcn)) {
+//                        // Parameter resolved by the container
+//                        $args[] = $container->get($rp_fqcn);
+//                    } elseif ($rp->isDefaultValueAvailable()) {
+//                        // Dependency with a default value provided
+//                        $args[] = $rp->getDefaultValue();
+//                    } elseif ($rp->allowsNull()) {
+//                        // Nullable dependency
+//                        $args[] = null;
+//                    } elseif (class_exists($rp_fqcn)) {
+//                        // Try instantating with argument-less constructor call
+//                        try {
+//                            $args[] = new $rp_fqcn();
+//                        } catch (Throwable $ex) {
+//                            throw new RuntimeException(
+//                                "Unable to instantiate an object for the parameter"
+//                                . " with name `{$rp_name}` and class `{$rp_fqcn}`"
+//                                . " for given callable!"
+//                            );
+//                        }
+//                    } else {
+//                        throw new RuntimeException(
+//                            "Unable to resolve the dependency parameter with name `{$rp_name}`"
+//                            . " for given callable!"
+//                        );
+//                    }
+//                } else {
+//                    throw new RuntimeException(
+//                        "`{$rp_fqcn}` is neither a valid interface nor a class name"
+//                        . " for given dependency named `{$rp_name}`!",
+//                    );
+//                }
+//            } elseif (isset($resolvedParams[$rp_name])) {
+//                // Injected parameter matched by name
+//                $args[] = $resolvedParams[$rp_name];
+//            } elseif ($container->has($rp_name)) {
+//                // Injected parameter resolved as container service-id
+//                $args[] = $container->get($rp_name);
+//            } elseif ($rp->isDefaultValueAvailable()) {
+//                // The use a default parameter, if available
+//                $args[] = $rp->getDefaultValue();
+//            } elseif ($rp->allowsNull()) {
+//                // Finally use the NULL value, if the parameter is nullable
+//                $args[] = null;
+//            } else {
+//                throw new RuntimeException(
+//                    "Unable to resolve the parameter with name `{$rp_name}` for given callable!",
+//                );
+//            }
+//        }
+//
+//        return $args;
+    }
+
+    /**
+     *
+     * @param string|array{0: object|string, 1: string}|object $callable
+     *      An [object/class, method] array expression, a function or an invokable
+     *      object. Use [fqcn, '__construct'] for class constructors.
+     * @return ReflectionParameters[]
+     * @throws RuntimeException
+     */
+    private function resolveReflectionParameters($callable): array
+    {
         // Check if closure or invokable objects
         $is_object    = is_object($callable);
         $is_closure   = $is_object && $callable instanceof Closure;
         $is_invokable = $is_object && !$is_closure && method_exists($callable, '__invoke');
 
-        // Only invokable objects are transformed into array form
-        // Closures are handled directly via reflection api
         if ($is_invokable) {
-            $callable = [$callable, '__invoke'];
-        }
-
-        // Fetch the reflection function/method parameters
-        if (is_array($callable)) {
+            // Try cached reflection parameters first, if any
+            $class = get_class($callable);
+            $cache_key = "{$class}::__invoke";
+            $rf_params = self::$rf_params[$cache_key] ?? null;
+            if ($rf_params === null) {
+                $rm = new ReflectionMethod($class, '__invoke');
+                self::$rf_params[$cache_key] = $rf_params = $rm->getParameters();
+            }
+        } elseif (is_array($callable)) {
             $object = $callable[0];
             $method = $callable[1];
             if (is_object($object)) {
@@ -79,17 +235,13 @@ class ParamsResolver implements ParamsResolverInterface
             $cache_key = "{$class}::{$method}";
             $rf_params = self::$rf_params[$cache_key] ?? null;
             if ($rf_params === null) {
-                if ($is_invokable) {
-                    $rm = new ReflectionMethod($class, '__invoke');
-                } else {
-                    $rc = new ReflectionClass($class);
-                    if (!$rc->hasMethod($method)) {
-                        throw new RuntimeException(
-                            "A method named `{$class}::{$method}` is not defined for given callable!"
-                        );
-                    }
-                    $rm = $method === '__construct' ? $rc->getConstructor() : $rc->getMethod($method);
+                $rc = new ReflectionClass($class);
+                if (!$rc->hasMethod($method)) {
+                    throw new RuntimeException(
+                        "A method named `{$class}::{$method}` is not defined for given callable!"
+                    );
                 }
+                $rm = $method === '__construct' ? $rc->getConstructor() : $rc->getMethod($method);
                 if ($rm instanceof ReflectionMethod) {
                     self::$rf_params[$cache_key] = $rf_params = $rm->getParameters();
                 }
@@ -113,6 +265,20 @@ class ParamsResolver implements ParamsResolverInterface
             return [];
         }
 
+        return $rf_params;
+    }
+
+    /**
+     * Resolve the argument values using injected params, the container dependencies
+     * and values, default values, if any, or the NULL value for nullable parameters
+     *
+     * @param ReflectionParameter[] $rf_params
+     * @param array|null $resolvedParams
+     * @return array<mixed>
+     * @throws RuntimeException
+     */
+    private function resolveArguments(array $rf_params, ?array $resolvedParams = null): array
+    {
         $container = $this->container;
 
         // Build the arguments for the provided ~callable~
